b5c53cb96f0273993352bcaa84ae2494895d5eed
omshinde0484 <shindeom052@gmail.com>
2025-08-28 05:53:12 -0700

    Fixed missing components import issue

--- app/page.js
+++ app/page.js
@@ -1,15 +1,15 @@
-import Hero from "@/components/Hero";
-import ServicesSection from "@/components/ServicesSection";
-import About from "@/components/About"
-import WhoWeAre from "@/components/WhoWeAre";
+import Hero from "../components/Hero";
+import ServicesSection from "../components/ServicesSection";
+import About from "../components/About";
+import WhoWeAre from "../components/WhoWeAre";
+import Works from "../components/Works";
+import Testimonial from "../components/Testimonial";
+import Faq from "../components/Faq";
+import Team from "../components/Team";
+import Contact from "../components/Contact";
+import Blog from "../components/Blog";
+import Footer from "../components/Footer";
 
-import Works from "@/components/Works"
-import Testimonial from "@/components/Testimonial"
-import Faq from "@/components/Faq"
-import Team from "@/components/Team"
-import Contact from "@/components/Contact"
-import Blog from "@/components/Blog"
-import Footer from "@/components/Footer"
 export default function Home() {
   return (
     <>
